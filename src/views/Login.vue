e41ac8ca31854e6ca53327f3a7e82fadfd24434c
ggarber42 <ggarber@protonmail.com>
2021-11-27 23:23:40 -0300

    fix: color

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
     <h1>Login</h1>
+    <login-form />
   </div>
 </template>
 
 <script>
+import LoginForm from '../components/LoginForm.vue'
+
 export default {
+  components: { LoginForm },
   name: 'Login',
 }
 </script>
 
 <style>
+.bm-overlay .card{
+      background: rgb(178 178 178);
+      border-color: rgb(178 178 178) !important;
+}
 </style>
