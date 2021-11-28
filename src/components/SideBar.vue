3c81d3e151480a6ef3078f3413a799040c38ef67
ggarber42 <ggarber@protonmail.com>
2021-11-27 23:21:52 -0300

    feat: create sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <slide width="250" noOverlay>
+    <router-link to="/" v-if="!isLoggedIn">Home</router-link>
+    <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
+    <router-link to="/signup" v-if="!isLoggedIn">Signup</router-link>
+    <router-link to="/logout" v-if="isLoggedIn">Logout</router-link>
+    <router-link to="/protected" v-if="isLoggedIn">Protected</router-link>
+    <router-link to="/about">About</router-link>
+  </slide>
+</template>
+
+<script>
+import { Slide } from 'vue-burger-menu'
+
+export default {
+  name: 'Sidebar',
+  components: {
+    Slide,
+  },
+  computed: {
+    isLoggedIn() { return this.$store.getters.user.loggedIn },
+  },
+}
+</script>
+
+<style>
+.bm-menu{
+  background-color: #fff;
+  border-right: 1px solid #dedede;
+}
+.bm-burger-button {
+    height: 20px;
+    width: 25px;
+    top: inherit;
+    left: 5vw;
+}
+</style>
