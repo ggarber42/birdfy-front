5ad3521735bb5cfb2ad91de782b60bd50269d45c
ggarber42 <ggarber@protonmail.com>
2021-11-27 23:23:04 -0300

    fix: use store for route guarding

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import firebase from 'firebase/compat/app'
+
+import store from '../store'
 import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
 import Login from '../views/Login.vue'
 import Protected from '../views/Protected.vue'
 
+// eslint-disable-next-line import/order
+import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
 Vue.use(VueRouter)
@@ -14,23 +17,46 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { requiresAuth: false },
     component: Home,
   },
   {
     path: '/login',
     name: 'Login',
+    meta: { requiresAuth: false },
     component: Login,
   },
   {
     path: '/signup',
     name: 'Signup',
+    meta: { requiresAuth: false },
     component: Signup,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    meta: { requiresAuth: true },
+    component: {
+      beforeRouteEnter(to, from, next) {
+        firebase
+          .auth()
+          .signOut()
+          .then(() => {
+            next('/')
+          })
+      },
+    },
   },
   {
     path: '/protected',
     name: 'Protected',
-    component: Protected,
     meta: { requiresAuth: true },
+    component: Protected,
   },
 ]
 
@@ -41,11 +67,12 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters.user.loggedIn
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
-  const isAuthenticated = firebase.auth().currentUser
-  console.log('isauthenticated', isAuthenticated)
   if (requiresAuth && !isAuthenticated) {
     next('/login')
+  } else if (!requiresAuth && isAuthenticated) {
+    next('/protected')
   } else {
     next()
   }
