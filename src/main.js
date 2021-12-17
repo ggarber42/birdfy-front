6be49cc9b6670ce872cb3c6dd3ca99879dca0d2f
ggarber42 <ggarber@protonmail.com>
2021-12-17 00:15:44 -0300

    fix: separete config file

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import firebase from 'firebase/compat/app'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebaseConfig from './firebaseConfig'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,15 +15,6 @@ Vue.config.devtools = true
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
-const firebaseConfig = {
-  apiKey: '',
-  authDomain: 'birdfy-auth-handler.firebaseapp.com',
-  projectId: 'birdfy-auth-handler',
-  storageBucket: 'birdfy-auth-handler.appspot.com',
-  messagingSenderId: '788117214849',
-  appId: '1:788117214849:web:9311a85f4a7b13927c9f80',
-}
 
 firebase.initializeApp(firebaseConfig)
 
